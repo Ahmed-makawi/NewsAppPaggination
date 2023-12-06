aefdf63b414b5c3b9bb6f7ff2f1e052aae88ba92
Ahmed-makawi <amakkawi17@gmail.com>
2023-12-06 02:49:29 +0200

    initial commit

---
+++ NewsApp/app/src/main/java/com/ahmed/composenewsapplication/presentation/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.gaur.composenewsapplication.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.gaur.composenewsapplication.presentation.screens.HomeScreen
+import com.gaur.composenewsapplication.ui.theme.ComposeNewsApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeNewsApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp {
+                        HomeScreen()
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MyApp(content: @Composable ()->Unit) {
+content()
+}
